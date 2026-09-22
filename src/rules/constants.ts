2a402cecdcb7da16e3311aca2cd17a33c1a36904
Ahmed Dghaies <dghaies.ahmed7@gmail.com>
2026-09-23 00:07:19 +0200

    new rule

--- src/rules/constants.ts
+++ src/rules/constants.ts
@@ -1,4 +1,5 @@
 export const RULES = {
+  BOOLEAN_PARAMETER_TRAP: "boolean-parameter-trap",
   NO_LARGE_FUNCTIONS: "no-large-functions",
   NO_DEEP_NESTING: "no-deep-nesting",
   NO_UNUSED_FILES: "no-unused-files",
